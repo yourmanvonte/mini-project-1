3bf0780b40fcd7d9138535af332cc382993a2669
Vonte <devonte.mote@gmail.com>
2025-05-12 21:23:58 -0700

    Add files via upload
    initial commit css, html, and Js

---
+++ IOD-mini-project-1/script.js
@@ -0,0 +1,37 @@
+// creating a random quote generator
+const quotes = [
+    { 
+        author: "Epictetus",
+        history: [
+            "Epictetus was a Greek Stoic philosopher who lived from 55 to 135 AD. He was born a slave in Phrygia, but later gained his freedom and became a prominent teacher of Stoicism. His teachings emphasized the importance of personal responsibility, self-discipline, and the power of the mind to shape one's experience of life. Epictetus believed that true happiness comes from within and that we should focus on what we can control rather than external circumstances.",
+        ],
+        quotes: {
+            quote: ["It's not what happens to you, but how you react to it that matters.", 
+                "You have power over your mind - not outside events. Realize this, and you will find strength.",
+                "Wealth consists not in having great possessions, but in having few wants.",
+                "No man is free, who is not master of himself.",
+                "Don't explain your philosophy. Embody it."],
+            source: "Epictetus",
+        }
+    },
+    {
+        author: "Marcus Aurelius",
+        quotes: {
+            quote: ["Waste no more time arguing about what a good man should be. Be one.",
+                "The best revenge is to be unlike him who performed the injury.",
+                "The obstacle is the way.",
+                "You have power over your mind - not outside events. Realize this, and you will find strength.",
+                "The happiness of your life depends upon the quality of your thoughts.",
+                "The universe is change; our life is what our thoughts make it.",
+                "It is not death that a man should fear, but he should fear never beginning to live.",
+                "Very little is needed for a happy life; it is all within yourself, in your way of thinking.",
+                "If it is not right, do not do it; if it is not true, do not say it.",
+                "He who lives in harmony with himself lives in harmony with the universe.",
+                "Death smiles at us all, but all a man can do is smile back.",
+            ],
+            source: "Marcus Aurelius",
+        }
+    }
+
+
+]
